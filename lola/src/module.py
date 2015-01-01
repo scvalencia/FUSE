7c6a3507f915954b1d4102845c2725bc5f11baf6
Sebastián Valencia <sc.valencia606@gmail.com>
2015-01-01 13:08:39 -0500

    Modules in lola

---
+++ lola/src/module.py
@@ -0,0 +1,62 @@
+class module(object):
+
+	def __init__(self, filename):
+
+		self.label = ''
+		self.ins = []
+		self.outs = []
+		self.modules = []
+		self.filename = filename
+		self.image = ''
+		self.done = False
+
+	def get_label(self):
+
+		return self.label
+
+	def get_inputs(self):
+
+		return self.ins
+
+	def get_outputs(self):
+
+		return self.outs
+
+	def get_filename(self):
+
+		return filename
+
+	def set_input(self, value):
+
+		if value not in [0, 1]:
+			raise ValueError("Pin value must be 0, or 1")
+
+		else:
+			self.ins.append(value)
+
+	def length_input(self):
+
+		return len(self.ins)
+
+	def length_output(self):
+
+		return len(self.outs)
+
+	def clean(self):
+
+		self.ins = []
+
+	def process_file():
+		pass
+
+class ModuleConnector(object):
+
+	def __init__(self, fmodule, tmodule):
+
+		self.fmodule = fmodule
+		self.tmodule = tmodule
+
+		self.f
+
+
+
